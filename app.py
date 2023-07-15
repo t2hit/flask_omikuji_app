d189388084408f5f98788905f58f08680fe2ad2b
t2hit <t.matsukusa@gmail.com>
2023-07-15 12:03:08 +0900

    おみくじアプリができた〜

---
+++ app.py
@@ -0,0 +1,17 @@
+import random
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+# おみくじアプリルーティング
+@app.route("/omikuji")
+def omikuji():
+    # random.choiceを使って大吉、吉、凶に振り分ける
+    result = random.choice(["大吉!!", "吉", "凶..."])
+    # テンプレートでresult変数を使用する
+    return render_template("omikuji.html", result=result)
+
+
+if __name__ == "__main__":
+    app.run(port=8000, debug=True)
